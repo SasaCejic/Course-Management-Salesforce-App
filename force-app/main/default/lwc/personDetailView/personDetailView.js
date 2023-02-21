9553451bef1c38d035b561d320e7fc9ca64fa70b
SasaCejic <cejicsasa17@gmail.com>
2023-02-21 01:32:25 +0100

    removed import of the Record_Type_Name field from PersonDetailView js file because I don't need it

--- force-app/main/default/lwc/personDetailView/personDetailView.js
+++ force-app/main/default/lwc/personDetailView/personDetailView.js
@@ -1,16 +1,11 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import PERSON_RECORD_TYPE from '@salesforce/schema/Person__c.Record_Type_Name__c';
 import PERSON_NAME_FIELD from '@salesforce/schema/Person__c.Name';
 
 export default class PersonDetailView extends LightningElement {
     @api recordId;
-    @wire(getRecord, {recordId: '$recordId', fields: [PERSON_RECORD_TYPE, PERSON_NAME_FIELD]})
-    person; 
-
-    get recordType(){
-        return getFieldValue(this.person.data, PERSON_RECORD_TYPE);
-    }
+    @wire(getRecord, {recordId: '$recordId', fields: [PERSON_NAME_FIELD]})
+    person;
 
     get name(){
         return getFieldValue(this.person.data, PERSON_NAME_FIELD);
