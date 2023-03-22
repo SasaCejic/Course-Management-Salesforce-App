96ac7aa95e2fa0034b24bcce0116174036b8283f
SasaCejic <cejicsasa17@gmail.com>
2023-03-22 10:14:28 +0100

    pulled metadata changes from org and deleted refresh function in aura component beacause it's not necessary

--- force-app/main/default/aura/auraLWCCreate/auraLWCCreateController.js
+++ force-app/main/default/aura/auraLWCCreate/auraLWCCreateController.js
@@ -8,9 +8,5 @@
         var recordTypeId = component.get("v.pageReference").state.recordTypeId;
         component.set("v.selectedRecordTypeId", recordTypeId);
         $A.get('e.force:refreshView').fire();
-    },
-
-    refresh : function(component, event, helper){
-        $A.get('e.force:refreshView').fire();
     }
 })
