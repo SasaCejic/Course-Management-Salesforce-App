4f20a50a3a28b8d8bdf313da3dc88b3e0af45f81
SasaCejic <cejicsasa17@gmail.com>
2023-03-26 16:52:59 +0200

    implemented logic to call an external API for phone validation and show the result, considered field access while showing the phone field, added validate, save and view details actions and enabled admin to set component visibility for each user

--- force-app/main/default/lwc/phoneValidationComponent/phoneValidationComponent.js
+++ force-app/main/default/lwc/phoneValidationComponent/phoneValidationComponent.js
@@ -1,5 +1,40 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import getNumberValidationKey from '@salesforce/apex/CustomMetadataFetcher.getNumberValidationKey';
 
 export default class PhoneValidationComponent extends LightningElement {
     @api phoneFieldName;
+    @api recordId;
+    @wire(getRecord, {recordId: '$recordId', fields: '$fieldsToRetrieve'})
+    person;
+    title;
+    fieldsToRetrieve;
+    validatedPhones = [];
+    fieldsToShow;
+    @wire(getNumberValidationKey)
+    numberValidationKey;
+
+    connectedCallback(){
+        this.title = `${this.phoneFieldName} Validation Component`;
+        this.fieldsToRetrieve = [`Person__c.${this.phoneFieldName}`];
+        this.fieldsToShow = [this.phoneFieldName];
+    }
+
+
+    get phoneValue(){
+        return this.person.data ? getFieldValue(this.person.data.fields, this.fieldsToRetrieve[0])[this.phoneFieldName].value : 'no data';
+    }
+
+    get fieldVisible(){
+        return this.person.data ? this.checkFieldVisibility(this.phoneFieldName) : false;
+    }
+
+    checkFieldVisibility(fieldName) {
+        return (this.person.data.fields[fieldName] != undefined) ? true : false;
+    }
+
+    validate(){
+        this.validatedPhones = (!this.validatedPhones.includes(this.phoneValue)) ? [...this.validatedPhones, this.phoneValue] : this.validatedPhones;
+    }
+
 }
